07d86fe6df4cfbddc2119b8d6d58560c5c51eb20
koki-mekonnen <ezanawittadele@gmail.com>
2022-10-08 16:29:35 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      tadaaa it is published
+    </div>
+  );
+}
+
+export default App;
